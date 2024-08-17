0bd8aa668580d9a5fa8440c0c587b756effa9662
wfcornelissen <wfcornelissen@gmail.com>
2024-08-17 20:18:28 +0200

    type create function finished

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 brand = str()
 model = str()
-type = tuple["1. Steer", "2. Pull", "3. Roll"]
+type = ["1. Steer", "2. Pull", "3. Roll"]
 state = tuple()
 starting_odo = int()
 serial = str()
 conf = str()
+tyre_db = {}
 
 
 
 def main():
     print("Hello World")
     tyre_creation()
-    
+
+
+
+#function to receive and store the brand of a specific tyre
 def brand_create():
     temp = input("Enter brand:")
     conf = input(f"You have entered {temp}. Is this correct? (y/n)")
@@ -24,6 +28,8 @@ def brand_create():
     else:
         print("Unexpected value. Exiting.")      
     return brand
+
+#function to receive and store the model of a specific tyre
 def model_create():
     temp = input("Enter model:")
     conf = input(f"You have entered {temp}. Is this correct? (y/n)")
@@ -36,23 +42,42 @@ def model_create():
     else:
         print("Unexpected value. Exiting.")
     return model
+
+#function to receive and store the type of a specific tyre
 def type_create():
     type = ["1. Steer", "2. Pull", "3. Roll"]
-    temp = int(input(f"Please choose type (1/2/3) {type}: "))
-    print (f'you have selected {temp}')
-    while temp != (1 or 2 or 3):
-        print("Invalid selection")
-        temp = input(f"Please choose type (1/2/3) {type}: ")
-    conf = input(f"You have selected {type[(temp -1)]}. Is this correct? (y/n)")
-    while conf != ("y" or "n"):
-        print("Invalid selection")
-        conf = input(f"You have selected {type[temp]}. Is this correct? (y/n)")
-    if conf == "y":
-        type = temp
+    print(type)
+    temp_type = int(input(f"Please select type from the above: "))
+    print(temp_type)
+    if temp_type == int(1):
+        conf_type = str(input(f"You have selected 1. Steer. Is this correct? (y/n)"))
+        if conf_type.lower() == "y":
+            print("Type saved")
+            type = conf_type
+            return type
+        else:
+            type_create()
+    elif temp_type == int(2):
+        conf_type = str(input(f"You have selected 2. Pull. Is this correct? (y/n)"))
+        if conf_type.lower() == "y":
+            print("Type saved")
+            type = conf_type
+            return type
+        else:
+            type_create()
+    elif temp_type == int(3):
+        conf_type = str(input(f"You have selected 3. Roll. Is this correct? (y/n)"))
+        if conf_type.lower() == "y":
+            print("Type saved")
+            type = conf_type
+            return type
+        else:
+            type_create()
     else:
-        print("You selected no. Returning to start.")
-        type_create()
-    return type
+        print("Error: Input invalid.")
+        type_create() 
+
+    pass
 
     
 #tyre_creation is intended to be used to add a tyre to the database of tyres and record its details.
